59e13fe2bef34962982dfb0fbc0a8f4e578c26c7
EdisonIsaiasReinosoM <edison1282015@gmail.com>
2025-10-16 23:48:50 -0500

    init commit

---
+++ src/main/java/com/example/demo/entities/Client.java
@@ -0,0 +1,23 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Clientes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String correo;
+    private String telefono;
+}
